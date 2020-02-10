b535921a54b57dc9a737aa97d305035da5eb2dca
Mirosław Buszek <zoowett@gmail.com>
2020-02-10 23:36:16 +0100

    Add payload structure

---
+++ src/main/java/com/wb/payload/ChangePasswordRequest.java
@@ -0,0 +1,17 @@
+package com.wb.payload;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+
+    @NotBlank
+    private String newPassword;
+    
+    @NotBlank
+    private String confirmPassword;
+}
