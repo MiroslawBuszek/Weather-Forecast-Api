c0063926f77aecc8cb684409b98ab10416e542ba
Mirosław Buszek <zoowett@gmail.com>
2020-03-06 23:24:56 +0100

    Add tests- Junit5

--- src/test/java/com/wb/test/WeatherForecastApiApplicationTests.java
+++ src/test/java/com/wb/test/WeatherForecastApiApplicationTests.java
@@ -1,78 +1,38 @@
 package com.wb.test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import com.wb.controller.WeatherForecastController;
-import com.wb.service.WeatherService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.wb.payload.Weather;
+import com.wb.utility.ApplicationConstants;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class WeatherForecastApiApplicationTests {
 
-	@Mock
-	private WeatherService weatherService;
-	@Mock
-	Future<Weather> futureWeather = new CompletableFuture<Weather>();
-	@InjectMocks
-	WeatherForecastController controller = new WeatherForecastController();
-	Weather weather;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Test
-	public void contextLoads() {}
+    @ParameterizedTest
+    @CsvFileSource(resources = "/findByCityCountry.csv", delimiter = ';')
+    public void findByCityCountry(String cityCountry) throws Exception {
+        mockMvc.perform(get(ApplicationConstants.WB_API_PRIFIX + "/wb/cityCountry/{cityCountry}", new Object[]{cityCountry}).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(matchAll(status().isOk()));
+    }
 
-	@Before
-	public void setUp() throws Exception {
-		weather = new Weather();
-		when(futureWeather.isDone()).thenReturn(true);
-		when(futureWeather.get()).thenReturn(weather);
-		when(weatherService.fetchWeather(Mockito.anyString())).thenReturn(weather);
-		when(weatherService.fetchWeatherForCity(Mockito.anyString())).thenReturn(futureWeather);
-		when(weatherService.fetchWeatherForCityByTime(Mockito.anyString(),Mockito.anyLong(),Mockito.anyLong())).thenReturn(futureWeather);
-	}
-
-	@Test
-	public void shouldReturnWeatherObject() throws InterruptedException, ExecutionException {
-		assertEquals(weather, controller.weatherInCity());
-	}
-
-	@Before
-	public void setUpFetchDataByTime() throws Exception {
-		weather = new Weather();
-		when(futureWeather.isDone()).thenReturn(true);
-		when(futureWeather.get()).thenReturn(weather);
-		when(weatherService.fetchWeatherByTime(Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong())).thenReturn(weather);
-		when(weatherService.fetchWeatherForCityByTime(Mockito.anyString(),Mockito.anyLong(), Mockito.anyLong())).thenReturn(futureWeather);
-	}
-
-	@Test
-	public void shouldReturnWeatherObjectFetchDataByTime() throws InterruptedException, ExecutionException {
-		assertEquals(weather, controller.weatherInCityByTime());
-	}
-
-	@Test(expected = InterruptedException.class)
-	public void shouldRaiseInterruptedException() throws InterruptedException, ExecutionException {
-		when(weatherService.fetchWeatherForCity(Mockito.anyString())).thenThrow(new InterruptedException());
-		controller.weatherInCity();
-	}
-
-	@Test(expected = InterruptedException.class)
-	public void shouldRaiseInterruptedExceptionForFetchDataByTime() throws InterruptedException, ExecutionException {
-		when(weatherService.fetchWeatherForCityByTime(Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong())).thenThrow(new InterruptedException());
-		controller.weatherInCityByTime();
-	}
+    @ParameterizedTest
+    @CsvFileSource(resources = "/findByCityCountryAndTime.csv", delimiter = ';')
+    public void findByCityCountryAndTime(String cityCountry, long startTimeStamp, long endTimeStamp) throws Exception {
+        mockMvc.perform(get(ApplicationConstants.WB_API_PRIFIX + "/wb/cityCountryAndTime?cityCountry={cityCountry}&startTimeStamp={startTimeStamp}&endTimeStamp={endTimeStamp}", new Object[]{cityCountry, startTimeStamp, endTimeStamp})
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(matchAll(status().isOk()));
+    }
 }
