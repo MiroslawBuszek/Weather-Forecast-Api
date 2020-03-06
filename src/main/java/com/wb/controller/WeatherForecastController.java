9b932cf43b756089a9203f2cc31c94fd2852fb43
Mirosław Buszek <zoowett@gmail.com>
2020-03-06 23:32:46 +0100

    Changes in controller for test cases

--- src/main/java/com/wb/controller/WeatherForecastController.java
+++ src/main/java/com/wb/controller/WeatherForecastController.java
@@ -1,9 +1,5 @@
 package com.wb.controller;
 
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import com.wb.payload.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wb.payload.ApiResponse;
 import com.wb.payload.Weather;
 import com.wb.service.WeatherService;
 import com.wb.utility.ApplicationConstants;
@@ -26,14 +23,13 @@ import io.swagger.annotations.ApiOperation;
 @Api(tags = "WB", value = "/wb")
 public class WeatherForecastController {
 
-    @Autowired
-    WeatherService weatherService;
+    @Autowired WeatherService weatherService;
 
     @ApiOperation(value = "Get Weather Data By City Country")
     @RequestMapping(value = "/cityCountry/{cityCountry}", method = RequestMethod.GET)
     ResponseEntity<?> cityCountry(@PathVariable("cityCountry") String cityCountry) {
         Weather weather = weatherService.fetchWeather(cityCountry);
-        return ResponseEntity.ok(new ApiResponse(true, ApplicationConstants.GET_KEY, Translator.toLocale("wb.data.by.city.country"), HttpStatus.OK.value(), weather));
+        return ResponseEntity.ok(new ApiResponse(true, ApplicationConstants.GET_KEY,Translator.toLocale("wb.data.by.city.country"), HttpStatus.OK.value(), weather));
     }
 
     @ApiOperation(value = "Get Weather Data By City Country & time")
@@ -42,23 +38,5 @@ public class WeatherForecastController {
         Weather weather = weatherService.fetchWeatherByTime(cityCountry, startTimeStamp, endTimeStamp);
         return ResponseEntity.ok(new ApiResponse(true, ApplicationConstants.GET_KEY,
                 Translator.toLocale("wb.data.by.city.country.time"), HttpStatus.OK.value(), weather));
-    }
-
-    //@RequestMapping(value = "/async/weatherInCity", method = RequestMethod.GET)
-    public Weather weatherInCity() throws InterruptedException, ExecutionException {
-        Future<Weather> dallas = weatherService.fetchWeatherForCity("Dallas,us");
-        // Wait until they are all done
-        while (!dallas.isDone()) {
-            Thread.sleep(10); // 10-millisecond pause between each check
-        }
-        return dallas.get();
-    }
-
-    public Weather weatherInCityByTime() throws InterruptedException, ExecutionException {
-        Future<Weather> dallas = weatherService.fetchWeatherForCityByTime("Dallas,us", 1369728000L, 1369789200L);
-        while (!dallas.isDone()) {
-            Thread.sleep(10); // 10-millisecond pause between each check
-        }
-        return dallas.get();
     }
 }
