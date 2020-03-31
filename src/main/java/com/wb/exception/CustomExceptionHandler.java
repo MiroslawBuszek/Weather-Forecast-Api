55a397c31dde1bb599f06b93fa4b3b937240556b
Mirosław Buszek <zoowett@gmail.com>
2020-03-31 23:11:54 +0200

    Handle exception

--- src/main/java/com/wb/exception/CustomExceptionHandler.java
+++ src/main/java/com/wb/exception/CustomExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.wb.exception;
 
+import java.net.ConnectException;
+import java.net.NoRouteToHostException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -19,5 +22,26 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
         List<String> errors = new ArrayList<>();
         errors.add(ex.getLocalizedMessage());
         return new ErrorResponse("PropertyNotFoundKey", errors, HttpStatus.NOT_FOUND.value());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ErrorResponse handleHttpClientErrorException(Exception ex, WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        errors.add(ex.getLocalizedMessage());
+        return new ErrorResponse("NotFound", errors, HttpStatus.NOT_FOUND.value());
+    }
+
+    @ExceptionHandler(ConnectException.class)
+    public ErrorResponse handleConnectException(Exception ex, WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        errors.add(ex.getLocalizedMessage());
+        return new ErrorResponse("TimeOut", errors, HttpStatus.REQUEST_TIMEOUT.value());
+    }
+
+    @ExceptionHandler(NoRouteToHostException.class)
+    public ErrorResponse handleNoRouteToHostException(Exception ex, WebRequest request) {
+        List<String> errors = new ArrayList<String>();
+        errors.add(ex.getLocalizedMessage());
+        return new ErrorResponse("NoRout", errors, HttpStatus.GATEWAY_TIMEOUT.value());
     }
 }
