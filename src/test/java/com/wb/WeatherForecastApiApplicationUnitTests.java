e012e4e29790005b8a2a3032d985aaf87783864b
Mirosław Buszek <zoowett@gmail.com>
2020-04-04 22:57:54 +0200

    Refactor unit test cases

--- src/test/java/com/wb/WeatherForecastApiApplicationUnitTests.java
+++ src/test/java/com/wb/WeatherForecastApiApplicationUnitTests.java
@@ -1,85 +1,54 @@
 package com.wb;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-
-import com.wb.controller.WeatherForecastController;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
 import com.wb.payload.WeatherMapper;
 import com.wb.service.WeatherService;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class WeatherForecastApiApplicationUnitTests {
-    @Mock
-    private WeatherService weatherService;
-    @Mock
-    Future<WeatherMapper> futureWeather = new CompletableFuture<>();
-    @InjectMocks
-    WeatherForecastController controller = new WeatherForecastController(weatherService);
-    WeatherMapper weatherMapper;
 
-    @org.junit.jupiter.api.Test
-    public void contextLoads() {
-    }
+    private final WeatherService weatherService;
+    private final MockMvc mockMvc;
 
-    @Before
-    public void setUp1() throws Exception {
-        weatherMapper = new WeatherMapper();
-        when(futureWeather.isDone()).thenReturn(true);
-        when(futureWeather.get()).thenReturn(weatherMapper);
-        when(weatherService.fetchWeatherByCity(Mockito.anyString())).thenReturn(weatherMapper);
-        when(weatherService.testWeatherByCity(Mockito.anyString())).thenReturn(futureWeather);
+    @Autowired
+    public WeatherForecastApiApplicationUnitTests(WeatherService weatherService, MockMvc mockMvc) {
+        this.weatherService = weatherService;
+        this.mockMvc = mockMvc;
     }
 
     @Test
-    public void shouldReturnCityWeatherObject() throws InterruptedException, ExecutionException {
-        Future<WeatherMapper> fw = weatherService.testWeatherByCity("Warsaw");
-        while (!fw.isDone()) {
-            Thread.sleep(10);
-        }
-        assertEquals(weatherMapper, fw.get());
-    }
-
-    @Before
-    public void setUp2() throws Exception {
-        weatherMapper = new WeatherMapper();
-        when(futureWeather.isDone()).thenReturn(true);
-        when(futureWeather.get()).thenReturn(weatherMapper);
-        when(weatherService.fetchWeatherByCityAndTime(Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong())).thenReturn(weatherMapper);
-        when(weatherService.testWeatherByCityAndTime(Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong())).thenReturn(futureWeather);
+    public void apiFetchWeatherByCity() throws Exception {
+        this.mockMvc.perform(get("/api/v1/wb/city/{city}", "Warsaw,pl").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(matchAll(status().isOk()));
     }
 
     @Test
-    public void shouldReturnCityTimeWeatherObject() throws InterruptedException, ExecutionException {
-        Future<WeatherMapper> fw = weatherService.testWeatherByCityAndTime("Warsaw", 1581712620L, 1581799020L);
-        while (!fw.isDone()) {
-            Thread.sleep(10);
-        }
-        assertEquals(weatherMapper, fw.get());
+    public void serviceFetchWeatherByCity() throws Exception {
+        WeatherMapper weatherMapper = weatherService.fetchWeatherByCity("Warsaw,pl");
+        Assertions.assertEquals(weatherMapper.getSys().getCountry(), "PL");
     }
 
-    @Test(expected = InterruptedException.class)
-    public void shouldRaiseInterruptedExceptionForFetchDataByCity() throws InterruptedException, ExecutionException {
-        when(weatherService.fetchWeatherByCity(Mockito.anyString())).thenThrow(new InterruptedException());
-        controller.weatherForCity("Warsaw");
+    @Test
+    public void apiFetchWeatherByCityAndTime() throws Exception {
+        this.mockMvc.perform(get("/api/v1/wb/cityAndTime?city={city}&startTimeStamp={startTimeStamp}&endTimeStamp={endTimeStamp}", "Warsaw,pl", 1581712620, 1581799020).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(matchAll(status().isOk()));
     }
 
-    @Test(expected = InterruptedException.class)
-    public void shouldRaiseInterruptedExceptionForFetchDataByTime() throws InterruptedException, ExecutionException {
-        when(weatherService.testWeatherByCityAndTime(Mockito.anyString(), Mockito.anyLong(), Mockito.anyLong())).thenThrow(new InterruptedException());
-        controller.weatherForCityAndTime("Warsaw", 1581712620L, 1581799020L);
+    @Test
+    public void serviceFetchWeatherByCityAndTime() throws Exception {
+        WeatherMapper weatherMapper = weatherService.fetchWeatherByCityAndTime("Warsaw,pl", 1581712620L, 1581799020L);
+        Assertions.assertEquals(weatherMapper.getSys().getCountry(), "PL");
     }
 }
