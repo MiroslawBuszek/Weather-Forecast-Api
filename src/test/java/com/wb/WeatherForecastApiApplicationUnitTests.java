47ba5433dbe7e3fa0a132b93e56f2b1e69163e64
Mirosław Buszek <zoowett@gmail.com>
2020-04-08 22:01:10 +0200

    Refactor test cases

--- src/test/java/com/wb/WeatherForecastApiApplicationUnitTests.java
+++ src/test/java/com/wb/WeatherForecastApiApplicationUnitTests.java
@@ -1,54 +1,99 @@
 package com.wb;
 
-import static org.springframework.test.web.servlet.ResultMatcher.matchAll;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withServerError;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpServerErrorException.InternalServerError;
 
 import com.wb.payload.WeatherMapper;
 import com.wb.service.WeatherService;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@ExtendWith(SpringExtension.class)
+
+@RestClientTest(WeatherService.class)
 public class WeatherForecastApiApplicationUnitTests {
 
-    private final WeatherService weatherService;
-    private final MockMvc mockMvc;
+    @Autowired
+    private WeatherService weatherService;
 
     @Autowired
-    public WeatherForecastApiApplicationUnitTests(WeatherService weatherService, MockMvc mockMvc) {
-        this.weatherService = weatherService;
-        this.mockMvc = mockMvc;
-    }
+    private MockRestServiceServer server;
 
-    @Test
-    public void apiFetchWeatherByCity() throws Exception {
-        this.mockMvc.perform(get("/api/v1/wb/city/{city}", "Warsaw,pl").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(matchAll(status().isOk()));
-    }
 
     @Test
     public void serviceFetchWeatherByCity() throws Exception {
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=0b65f02ed4ef88a3598934f2ae5346d5"))
+                .andRespond(withSuccess("{\"sys\": {\"country\":\"PL\"}}", MediaType.APPLICATION_JSON));
         WeatherMapper weatherMapper = weatherService.fetchWeatherByCity("Warsaw,pl");
         Assertions.assertEquals(weatherMapper.getSys().getCountry(), "PL");
     }
 
     @Test
-    public void apiFetchWeatherByCityAndTime() throws Exception {
-        this.mockMvc.perform(get("/api/v1/wb/cityAndTime?city={city}&startTimeStamp={startTimeStamp}&endTimeStamp={endTimeStamp}", "Warsaw,pl", 1581712620, 1581799020).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(matchAll(status().isOk()));
-    }
-
-    @Test
     public void serviceFetchWeatherByCityAndTime() throws Exception {
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=0b65f02ed4ef88a3598934f2ae5346d5&type=hour&start=1581712620&end=1581799020"))
+                .andRespond(withSuccess("{\"sys\": {\"country\":\"PL\"}}", MediaType.APPLICATION_JSON));
         WeatherMapper weatherMapper = weatherService.fetchWeatherByCityAndTime("Warsaw,pl", 1581712620L, 1581799020L);
         Assertions.assertEquals(weatherMapper.getSys().getCountry(), "PL");
     }
+
+    @Test
+    public void response4xxForWeatherByCity() throws Exception {
+
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl111&appid=0b65f02ed4ef88a3598934f2ae5346d5"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND));
+        HttpClientErrorException exception = Assertions.assertThrows(HttpClientErrorException.class, () -> {
+            weatherService.fetchWeatherByCity("Warsaw,pl111");
+        });
+        Assertions.assertTrue(exception.getMessage().contains("404 Not Found"));
+    }
+
+    @Test
+    public void response4xxForWeatherByCityAndTime() throws Exception {
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl111&appid=0b65f02ed4ef88a3598934f2ae5346d5&type=hour&start=1581712620&end=1581799020"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND));
+        HttpClientErrorException exception = Assertions.assertThrows(HttpClientErrorException.class, () -> {
+            weatherService.fetchWeatherByCityAndTime("Warsaw,pl111", 1581712620L, 1581799020L);
+        });
+        Assertions.assertTrue(exception.getMessage().contains("404 Not Found"));
+    }
+
+    @Test
+    public void response5xxForWeatherByCity() {
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=0b65f02ed4ef88a3598934f2ae5346d5"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withServerError());
+        InternalServerError exception = Assertions.assertThrows(InternalServerError.class, () -> {
+            weatherService.fetchWeatherByCity("Warsaw,pl");
+        });
+        Assertions.assertTrue(exception.getMessage().contains("500 Internal Server Error"));
+    }
+
+    @Test
+    public void response5xxForWeatherByCityAndTime() throws Exception {
+        this.server.expect(requestTo("http://api.openweathermap.org/data/2.5/weather?q=Warsaw,pl&appid=0b65f02ed4ef88a3598934f2ae5346d5&type=hour&start=1581712620&end=1581799020"))
+                .andExpect(method(HttpMethod.GET))
+                .andRespond(withServerError());
+        InternalServerError exception = Assertions.assertThrows(InternalServerError.class, () -> {
+            weatherService.fetchWeatherByCityAndTime("Warsaw,pl", 1581712620L, 1581799020L);
+        });
+        Assertions.assertTrue(exception.getMessage().contains("500 Internal Server Error"));
+    }
 }
+
+
